b067f2e365e65dea91762017b1884a65cdc5e84e
BonDizarD <8123bharathbond8123@gmail.com>
2025-05-25 18:29:46 +0530

    Local Server setup

---
+++ local_server_setup/src/script.js
@@ -0,0 +1,9 @@
+/**
+ * Author: Bharath Kumar S
+ * Date Created: 2025-05-25
+ * Title: Local Server Setup
+ * Description: Setup file can be used as a starter project for three.js
+ */
+
+import * as THREE from "three";
+console.log(THREE);
